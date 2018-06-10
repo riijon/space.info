1e155a21a78cab7d506579f974e3c8484db8829a
RikuTakenaka <riku@ranaextractive.com>
2018-06-11 00:38:45 +0900

    使わないテーマファイルを削除

--- wp-content/themes/space_hc/page-spacehcpage.php
+++ wp-content/themes/space_hc/page-spacehcpage.php
@@ -12,7 +12,7 @@ get_header();
 date_default_timezone_set('Asia/Tokyo');
 
 ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <div class="page-top wrap">
     <h1 class="top-desc rl"><?php the_title(); ?></h1>
   </div>
@@ -26,50 +26,49 @@ date_default_timezone_set('Asia/Tokyo');
     <div class="content">
       <div class="flex">
         <div class="item">
-            <?php
-            //画像(返り値は「画像ID」)
-            $img = get_field('page_img_1');
-            $imgurl = wp_get_attachment_image_src($img, 'large'); //サイズは自由に変更してね
-            if($imgurl){ ?><img src="<? echo $imgurl[0]; ?>" alt="">
-            <? }
-            ?>
+          <?php
+          //画像(返り値は「画像ID」)
+          $img = get_field('page_img_1');
+          $imgurl = wp_get_attachment_image_src($img, 'large'); //サイズは自由に変更してね
+          if ($imgurl) { ?><img src="<? echo $imgurl[0]; ?>" alt="">
+          <?php }
+          ?>
           <?php the_field('page_img_1_text'); ?>
         </div>
         <div class="item">
-            <?php
-            //画像(返り値は「画像ID」)
-            $img = get_field('page_img_2');
-            $imgurl = wp_get_attachment_image_src($img, 'large'); //サイズは自由に変更してね
-            if($imgurl){ ?><img src="<? echo $imgurl[0]; ?>" alt="">
-            <? }
-            ?>
-            <?php the_field('page_img_2_text'); ?>
+          <?php
+          //画像(返り値は「画像ID」)
+          $img = get_field('page_img_2');
+          $imgurl = wp_get_attachment_image_src($img, 'large'); //サイズは自由に変更してね
+          if ($imgurl) { ?><img src="<? echo $imgurl[0]; ?>" alt="">
+          <?php }
+          ?>
+          <?php the_field('page_img_2_text'); ?>
         </div>
         <div class="item">
-            <?php
-            //画像(返り値は「画像ID」)
-            $img = get_field('page_img_3');
-            $imgurl = wp_get_attachment_image_src($img, 'large'); //サイズは自由に変更してね
-            if($imgurl){ ?><img src="<? echo $imgurl[0]; ?>" alt="">
-            <? }
-            ?>
-            <?php the_field('page_img_3_text'); ?>
+          <?php
+          //画像(返り値は「画像ID」)
+          $img = get_field('page_img_3');
+          $imgurl = wp_get_attachment_image_src($img, 'large'); //サイズは自由に変更してね
+          if ($imgurl) { ?><img src="<? echo $imgurl[0]; ?>" alt="">
+          <?php }
+          ?>
+          <?php the_field('page_img_3_text'); ?>
         </div>
         <div class="item">
-            <?php
-            //画像(返り値は「画像ID」)
-            $img = get_field('page_img_4');
-            $imgurl = wp_get_attachment_image_src($img, 'large'); //サイズは自由に変更してね
-            if($imgurl){ ?><img src="<? echo $imgurl[0]; ?>" alt="">
-            <? }
-            ?>
-            <?php the_field('page_img_4_text'); ?>
+          <?php
+          //画像(返り値は「画像ID」)
+          $img = get_field('page_img_4');
+          $imgurl = wp_get_attachment_image_src($img, 'large'); //サイズは自由に変更してね
+          if ($imgurl) { ?><img src="<? echo $imgurl[0]; ?>" alt="">
+          <?php }
+          ?>
+          <?php the_field('page_img_4_text'); ?>
         </div>
       </div>
     </div>
   </div>
-<?php endwhile; else : ?>
-  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endwhile; ?>
 <?php endif; ?>
 <?php
 get_footer();
